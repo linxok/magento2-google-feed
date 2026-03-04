931a33568b8d85630a5c16e4aae358a868bfc476
linxok <linxok@gmail.com>
2026-03-05 00:31:16 +0200

    my modele feed

--- view/adminhtml/web/js/form/element/google-category-tree.js
+++ view/adminhtml/web/js/form/element/google-category-tree.js
@@ -3,12 +3,41 @@ define([
 ], function (Abstract) {
     'use strict';
 
+    var TMPL_ID = 'gc-category-picker-tmpl';
+
+    function injectTemplate() {
+        if (document.getElementById(TMPL_ID)) {
+            return;
+        }
+        var s = document.createElement('script');
+        s.type = 'text/html';
+        s.id   = TMPL_ID;
+        s.text = [
+            '<input type="hidden"',
+            '    data-bind="value: value, attr: {name: inputName, id: uid}"/>',
+            '<input class="admin__control-text" type="text" readonly="readonly"',
+            '    style="width:auto;min-width:260px;cursor:default;background:#f9f9f9;"',
+            '    data-bind="value: displayLabel,',
+            '        attr: {id: uid + \'_display\', placeholder: $t(\'No category selected\')}"',
+            '/>',
+            '<button type="button" class="action-default scalable" style="margin-left:6px;"',
+            '    data-bind="click: openPickerWindow, i18n: \'Select Category\'">',
+            '</button>',
+            '<!-- ko if: value() -->',
+            '<button type="button" class="action-default scalable" style="margin-left:4px;"',
+            '    data-bind="click: clearValue, i18n: \'Clear\'">',
+            '</button>',
+            '<!-- /ko -->'
+        ].join('\n');
+        document.head.appendChild(s);
+    }
+
     return Abstract.extend({
 
         defaults: {
             pickerUrl: '',
             displayLabel: '',
-            elementTmpl: 'MyCompany_GoogleFeed/form/element/google-category-picker',
+            elementTmpl: TMPL_ID,
             listens: {}
         },
 
@@ -20,6 +49,7 @@ define([
         },
 
         initialize: function () {
+            injectTemplate();
             this._super();
 
             var self = this;
